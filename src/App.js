bdce3e54b3cd06f60c0fd14bbc6dfb41fb2ac101
Digambaran <arjunsreekumar.1@gmail.com>
2022-05-27 13:17:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import e_candidate_message from './e_candidate_message'
+export default function App() {
+  return (
+    <div>
+      <e_candidate_message/>
+    </div>
+  )
+}
---
+++ src/e_candidate_message.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const e_candidate_message = () => {
+  return (
+   <p>Hello from e_candidate_message </p>
+  )
+}
+export default e_candidate_message
